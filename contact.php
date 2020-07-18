7260d7b76f348aabcd8b2b55982f3c1e2aeaa8ba
Hibou2020 <68447655+Hibou2020@users.noreply.github.com>
2020-07-18 14:35:41 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,46 @@
+ 
+ <?php
+ 
+ 
+        
+        
+    try
+        {
+            
+           $connexion = new PDO('mysql:host=localhost; dbname=domino', 'root','');
+        }
+            
+    catch(Exception $e)
+    {
+            
+       die('Erreur :'.$e->getMessage());
+    }
+            
+    $pdoconnect = $connexion->prepare('INSERT INTO contact(nom,mel,mesg) VALUES ( :nom, :mel, :mesg)');
+            
+    $pdoconnect->execute(array('nom'=>$_POST['Name'], 'mel'=>$_POST['mail'],'mesg'=>$_POST['mssg'],));
+
+    $insertIsOk = $pdoconnect;
+    if ($insertIsOk)
+        {
+            $message = 'Votre requete a été bien éfectué. ';
+        }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" /> 
+    <title></title>
+</head>
+<body>
+    <h1>INSCRIPTION</h1>
+    <p>
+        <?php
+            echo $message;
+        ?>
+    </p>
+
+</body>
+</html>
