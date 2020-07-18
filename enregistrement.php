80713f4d7bd9ca8a0c6b35a717f8f11208093d47
Hibou2020 <68447655+Hibou2020@users.noreply.github.com>
2020-07-18 16:09:08 +0200

    Update enregistrement.php

--- enregistrement.php
+++ enregistrement.php
@@ -7,7 +7,7 @@
     try
         {
             
-           $connexion = new PDO('mysql:host=localhost; dbname=id14368008_domino', 'id14368008_root','');
+           $connexion = new PDO('mysql:host=localhost; dbname= 'domino', 'root','@Groupe06admin');
         }
             
     catch(Exception $e)
